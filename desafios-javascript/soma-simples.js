a7d29d2e516704666121f9dbb8f2d798c1c1d35f
Ju Nunes <jucilenenunes10@gmail.com>
2021-10-07 19:27:40 -0300

    Resolvendo mais exercícios de Javascript do Bootcamp da Eduzz da Digital Innovation One.

---
+++ desafios-javascript/soma-simples.js
@@ -0,0 +1,32 @@
+/*Desafio
+Leia dois valores inteiros identificados como variáveis A e B. Calcule a soma entre elas e chame essa variável de SOMA.
+A seguir escreva o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B.
+
+ 
+Exemplos de Entrada	Exemplos de Saída
+30
+10
+
+SOMA = 40
+
+-30
+10
+
+SOMA = -20
+
+0
+0
+
+SOMA = 0*/
+
+let A = parseInt(gets(30));
+let B = parseInt(gets(10));
+let SOMA = A + B  ;               //Preencha os espaços em branco
+print("SOMA = "+  SOMA   );    //Preencha os espaços em branco
+
